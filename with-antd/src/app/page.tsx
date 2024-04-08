4eb7ad23c3577627ffe4a306c4fc78df1016a3dd
tuntun <760523867@qq.com>
2024-04-08 15:03:35 +0800

    feat: with antd

---
+++ with-antd/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+import { VerticalAlignBottomOutlined } from '@ant-design/icons'
+
+export default function Home() {
+  return (
+    <main>
+      <Button icon={<VerticalAlignBottomOutlined />}>test</Button>
+    </main>
+  )
+}
